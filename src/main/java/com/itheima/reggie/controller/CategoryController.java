e81193da4b5e4a7758f5db6bd4843d5877a966eb
jiahao798 <z1397893423@qq.com>
2024-07-15 18:09:07 +0800

    菜品套餐分类的新增，删除，修改以及页面分页查询功能

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,15 +1,14 @@
 package com.itheima.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -22,9 +21,46 @@ public class CategoryController {
     * 新增分类
     * */
     @PostMapping
-    public R<String> save(@RequestBody Category category) {
+    public R<String> save(Category category) {
         log.info("category: {}", category);
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+    /*
+    * 分页查询
+    * */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        //分页构造器
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+        //进行分页查询
+        categoryService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
+
+    /*
+    * 根据id删除分类
+    * */
+    @DeleteMapping
+    public R<String> delete(Long ids){
+        log.info("删除分类，id: {}", ids);
+
+//        categoryService.removeById(ids);
+        categoryService.remove(ids);
+        return R.success("分类信息删除成功");
+    }
+
+    @PutMapping
+    public R<String> update(@RequestBody Category category) {
+        log.info("修改分类信息: {}", category);
+
+        categoryService.updateById(category);
+        return R.success("修改分类信息成功");
     }
 }
